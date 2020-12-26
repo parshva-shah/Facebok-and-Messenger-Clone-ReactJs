7fbc89f3ed45e8e17a1c247e788b9f7f34109f52
Parshva Shah <parshva.shah@sakec.ac.in>
2020-12-26 11:46:20 +0530

    Add files via upload

---
+++ build/precache-manifest.11f458444947797a0dd993577238e0d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec21f8ea2e6342906126c3ec81d4aef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9f2cd8adceadf31c7c",
+    "url": "/static/css/main.938073ea.chunk.css"
+  },
+  {
+    "revision": "34f2ffa5224aeb70b7a2",
+    "url": "/static/js/2.f9b0dec6.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.f9b0dec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9f2cd8adceadf31c7c",
+    "url": "/static/js/main.f5a702e9.chunk.js"
+  },
+  {
+    "revision": "a9bcb2082ea44ffc1df4",
+    "url": "/static/js/runtime-main.ec8f8518.js"
+  }
+]);
